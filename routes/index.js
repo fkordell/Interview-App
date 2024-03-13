9dafa425285b4335924282ab7708e7dec1743d36
fkordell <far18009@byui.edu>
2024-03-13 17:44:42 -0600

    interviews update

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,6 @@ const router = express.Router();
 router.use("/", require("./swagger"));
 router.use("/companies", require("./companies"));
 router.use("/users", require("./users"));
-router.use('/interviews', require('./interviews'));
+// router.use('/interviews', require('./interviews'));
 
 module.exports = router;
